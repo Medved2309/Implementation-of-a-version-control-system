7af3873657f5175cbadf56bf5ec656594141a957
Andrey Dmitrievich <medved2309@gmail.com>
2022-10-14 14:11:29 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] Sales = {1, 2, 4, 100, 10, 20, 30};
+        SalesManager salesManager = new SalesManager(Sales);
+        System.out.println(salesManager.max());
+
+    }
+}
+
+
+
+
